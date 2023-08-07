67dc176539901f78a8548748c26e06a3a253f726
Mohd Yusuf Parvez <codervilgax@gmail.com>
2023-08-07 22:45:37 +0530

    Day 2

---
+++ Day 2/LeftRightShift.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main( ) {
+    int a = 4;
+    int b = 6;
+    // left shift 
+    cout << (a << 1) <<endl ;
+    cout << (b << 1) <<endl ;
+
+    // it acts like multiplying by 2 in this case
+
+    // right shift 
+
+    cout << (a >> 1) <<endl;
+    cout << (b >> 1) <<endl;
+
+    // it acts like divide by 2 in this case
+
+}
